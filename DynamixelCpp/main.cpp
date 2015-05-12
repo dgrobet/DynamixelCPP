f10ac47564db09b9c17c5df8b24334816ec76631
dgrobet <grobet@isib.be>
2015-05-12 09:34:29 +0200

    Initial Commit

---
+++ DynamixelCpp/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DynamixelCpp
+//
+//  Created by damien grobet on 12/05/15.
+//  Copyright (c) 2015 damien grobet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
